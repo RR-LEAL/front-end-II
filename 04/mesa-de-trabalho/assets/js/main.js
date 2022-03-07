d2decdf4e951b9da50baba69b0e04f2b25d91b2c
Rodrigo Rodrigues Leal da Silva <63212120+RR-LEAL@users.noreply.github.com>
2022-03-07 20:38:12 -0300

    aula 4

---
+++ 04/mesa-de-trabalho/assets/js/main.js
@@ -0,0 +1,17 @@
+var objeto = document.getElementsByTagName("li");
+
+objeto[0].classList.add('removido');
+objeto[0].innerText = "Carro - Removido";
+
+objeto[1].classList.add('selecionado');
+objeto[1].innerText = "Bike - Selecionado";
+
+objeto[2].classList.add('selecionado');
+objeto[2].innerText = "Patenete - Selecionado";
+
+
+objeto[3].classList.add('removido');
+objeto[3].innerText = "Celular - Removido";
+
+objeto[4].classList.add('removido');
+objeto[4].innerText = "Carregador- Removido";
